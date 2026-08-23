fb582de47328c07aa89bcca44b0170dc7d58d14c
Karrang <muhammadkarrangtungku@gmail.com>
2026-08-23 19:37:11 +0700

    Add home and project detail views.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
+import ExperienceSection from '@/components/ExperienceSection.vue'
+import SkillsSection from '@/components/SkillsSection.vue'
+import ProjectsSection from '@/components/ProjectsSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
+</script>
+
+<template>
+  <HeroSection />
+  <AboutSection />
+  <ExperienceSection />
+  <SkillsSection />
+  <ProjectsSection />
+  <ContactSection />
+</template>
